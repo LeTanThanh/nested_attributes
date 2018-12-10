fc22dc86d1c66c6574f7af68beb710609378fbe2
LeTanThanh <tanthanhcr95@gmail.com>
2018-12-10 09:22:24 +0700

    add_model_director

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,3 @@
 class Company < ApplicationRecord
+  has_one :director, dependent: :destroy
 end
---
+++ db/migrate/20181210022030_create_directors.rb
@@ -0,0 +1,10 @@
+class CreateDirectors < ActiveRecord::Migration[5.2]
+  def change
+    create_table :directors do |t|
+      t.string :name
+      t.references :company, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
